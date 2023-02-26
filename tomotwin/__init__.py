9c4e768826630faa7078d89cd6936fc159873488
Grigory Sharov <sharov.grigory@gmail.com>
2023-02-26 12:38:01 +0000

    fix plugin url

--- tomotwin/__init__.py
+++ tomotwin/__init__.py
@@ -39,7 +39,7 @@ _logo = "tomotwin_logo.png"
 
 class Plugin(pwem.Plugin):
     _pathVars = [TOMOTWIN_MODEL]
-    _url = "https://tomotwin-cryoet.readthedocs.io/"
+    _url = "https://github.com/scipion-em/scipion-em-tomotwin"
     _supportedVersions = VERSIONS
 
     @classmethod
